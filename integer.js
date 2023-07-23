4d1952b9e9d22c121fa94104b5687f7a45620690
ThuyDuong97TD <139732590+ThuyDuong97TD@users.noreply.github.com>
2023-07-23 12:15:07 +0700

    them

--- integer.js
+++ integer.js
@@ -2,13 +2,31 @@ let arr1 = ["1234567890234567890234567890"];
 let arr2 = ["987654321198765432119876543211"];
 parseInt(arr1);
 parseInt(arr2);
-// console.log((arr + arr2));
-if (arr || arr2 >= 1000000000000000000000000)
+
+function check(arr1, arr2) {
+  if (arr1 || arr2 >= 1000000000000000000000000) {
     console.log("ok");
-    else console.log("no");
+  } else {
+    console.log("no");
+  }
+}
+ function sum(arr1, arr2) {
+    let sum = 0;
+    for (let i = 0; i < arr1.length; i++) {
+      sum += arr1[i] + arr2[i];
+    }
+    return sum;
+  }
+    console.log(sum(arr1, arr2));
 
-
-
+ function multiply (arr1, arr2) {
+    let multiply = 0;
+    for (let i = 0; i < arr1.length; i++) {
+      multiply += arr1[i] + arr2[i];
+    }
+    return multiply;
+  }
+    console.log(multiply(arr1, arr2));
 
 
 
