edd6f1fcd089c3b61a7fc04e2d5f765c2425330b
ThuyDuong97TD <139732590+ThuyDuong97TD@users.noreply.github.com>
2023-07-23 12:00:14 +0700

    integer

---
+++ integer.js
@@ -0,0 +1,25 @@
+let arr1 = ["1234567890234567890234567890"];
+let arr2 = ["987654321198765432119876543211"];
+parseInt(arr1);
+parseInt(arr2);
+// console.log((arr + arr2));
+if (arr || arr2 >= 1000000000000000000000000)
+    console.log("ok");
+    else console.log("no");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
